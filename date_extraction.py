ae247595a7f0bca62e0f51095db6b4c7a7765a3e
Mohd Numan <100748027+numanekak1@users.noreply.github.com>
2022-04-18 11:19:07 +0530

    Create date_extraction.py
    Date Extraction

---
+++ date_extraction.py
@@ -0,0 +1,10 @@
+# __2022_04_09__attendance.csv
+
+a='__2022_04_09__attendance.csv'
+b=[]
+b=a.split('_')
+# print(b)
+dd,mm,yyyy=b[4],b[3],b[2]
+# print(dd+'-'+mm+'-'+yyyy)
+output_date=dd+'-'+mm+'-'+yyyy
+print(output_date)
